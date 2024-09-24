940e6bb03469699f994dacec9123e5189dc4de5c
shunya-fug <shunnya0715@gmail.com>
2024-09-24 23:35:27 +0900

    🎉

---
+++ src/main/kotlin/com/dean/ezuml/EzumlApplication.kt
@@ -0,0 +1,11 @@
+package com.dean.ezuml
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EzumlApplication
+
+fun main(args: Array<String>) {
+	runApplication<EzumlApplication>(*args)
+}
---
+++ src/test/kotlin/com/dean/ezuml/EzumlApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.dean.ezuml
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EzumlApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
